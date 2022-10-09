412e44ebedb04b926bcf3b66005f1bc257b0c9bc
chanjunpark <wns0415@gmail.com>
2022-10-09 23:12:00 +0900

    실전 예제 3

---
+++ jpa-exercise-shop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+
+}
--- jpa-exercise-shop/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpa-exercise-shop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Item {
@@ -12,6 +14,9 @@ public class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
     public Item(String name) {
         this.name = name;
